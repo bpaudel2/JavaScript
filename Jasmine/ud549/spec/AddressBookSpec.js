cbadf02eff4e4b6d2b33a5a90ef5d463f46fda92
bpaudel2 <beekpal@gmail.com>
2019-02-17 21:52:07 -0600

    Add files via upload

---
+++ Jasmine/ud549/spec/AddressBookSpec.js
@@ -0,0 +1,31 @@
+describe('Address Book', function(){ //Suite
+    var addressBook,
+        thisContact;
+    beforeEach(function(){
+        addressBook = new AddressBook(),
+        thisContact = new Contact();
+    });
+    it('should be able to add a contanct', function(){ //Spec
+        addressBook.addContact(thisContact);
+        expect(addressBook.getContact(0)).toBe(thisContact);
+
+    });
+    it('should be able to delete a contact', function(){
+        addressBook.addContact(thisContact);
+        addressBook.deleteContact(0);
+        expect(addressBook.getContact(0)).not.toBeDefined();
+    })
+});
+
+describe('Async Address Book', function(){
+    var addressBook = new AddressBook();
+    beforeEach(function(done){
+        addressBook.getInitialContacts(function(){
+            done();
+        });
+    });
+    it('should grab initial contacts', function(done){
+        expect(addressBook.initialComplete).toBe(true);
+        done();
+    });
+})
